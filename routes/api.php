dc2c9e4e3e2a9e1a0343f4c8e510c13377e368d7
derderian7 <100807843+derderian7@users.noreply.github.com>
2024-05-17 20:49:52 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,48 @@
 <?php
 
-
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BookingsController;
-use App\Http\Controllers\Business\ServiceController;
-use App\Http\Controllers\ReviewsController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\Business\BookingController as BusinessBookingController;
+use App\Http\Controllers\Business\ServicesController;
+use App\Http\Controllers\Business\TransactionsController;
+use App\Http\Controllers\BusinessController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ReviewsController;
+use App\Http\Controllers\NotificationSendController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::post('login',[AuthController::class,'login']);
-Route::post('register',[AuthController::class,'register']);
-
-Route::middleware('auth:sanctum')->group(function(){
-    Route::apiResource('service',ServiceController::class);
-    Route::apiResource('booking',BookingsController::class);
-    Route::post('update_service/{id}',[ServiceController::class,'update']);
-    Route::apiResource('review',ReviewsController::class);
-    Route::get('business_reviews/{id}',[ReviewsController::class,'business_review']);
-    Route::post('update_review/{id}',[ReviewsController::class,'update']);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
+Route::middleware(['auth:sanctum'])->group(function () {
+Route::resource('service',ServicesController::class);
+Route::get('business_services',[ServicesController::class,'business_services']);
+Route::post('update_service/id',[ServicesController::class,'update_service']);
+Route::resource('reviews',ReviewsController::class);
+Route::resource('business',BusinessController::class);
+Route::resource('booking',BookingController::class);
+Route::get('user_bookings',[BookingController::class,'user_bookings']);
+Route::get('business_bookings',[BusinessBookingController::class,'business_bookings']);
+Route::get('getAvailableHours/{serviceId}/{selectedDate}',[BookingController::class,'getAvailableHours']);
+Route::get('services_types',[BusinessController::class,'services_types']);
+Route::get('business_details',[BusinessController::class,'business_details']);
+Route::post('change_booking_status/{id}',[BusinessBookingController::class,'change_booking_status']);
+Route::post('change_booking_price/{id}',[BusinessBookingController::class,'change_booking_price']);
+Route::post('update_business',[ServicesController::class,'business_services']);
+Route::post('update_fcm_token', [NotificationSendController::class, 'updateDeviceToken']);
 
-Route::post('update_business/{id}',[BusinessController::class,'update']);
+Route::get('transactions',[TransactionsController::class,'index']);
 
-Route::get('/auth', function () {
-    return response()->json(['message'=>'please login first']);
-})->name('auth');
+Route::get('remaining',[TransactionsController::class,'remaining']);
+
+Route::post('/send-web-notification', [NotificationSendController::class, 'sendNotification']);
+
+});
